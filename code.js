bd4b4049a5c7ae275159bbff57b5baf51b2fc9e8
Matteo Speranza <matteo.u.speranza@gmail.com>
2023-11-15 13:52:10 -0600

    added function sayHello and started function isFive

--- code.js
+++ code.js
@@ -2,3 +2,15 @@
 function helloWorld() {
     return "Hello, World!";
 }
+function sayHello(personName) {
+    if (typeof personName === `string`) {
+        return `Hello, ${personName}`
+    } else if (typeof personName === `boolean`) {
+        return "Hello, World"
+    } else {
+        return "Hello, Matteo!"
+    }
+}
+function isFive(inputOfFive) {
+    return inputOfFive
+}
