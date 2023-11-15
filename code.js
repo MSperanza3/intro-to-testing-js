1dee10648db72e01645627c85e4a56dab10998b7
Matteo Speranza <matteo.u.speranza@gmail.com>
2023-11-15 16:22:31 -0600

    started isVowel

--- code.js
+++ code.js
@@ -15,5 +15,11 @@ function isFive(input1) {
     if (input1 === 5 || '5') {
         return true
     }
-    return typeof input === 'boolean'
+    return typeof input1 === 'boolean'
+}
+function isEven(input1) {
+    return input1 % 2 === 0;
+}
+function isVowel(vowel) {
+    return !!(vowel === `a` || `e` || `i` || `o` || `u`);
 }
